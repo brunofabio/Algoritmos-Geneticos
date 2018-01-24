c08d36392b2f5bd31a899218264ad3a8b6707139
brunofabio <bbedonv@uni.pe>
2018-01-24 17:32:07 -0500

    Commit inicial

---
+++ ModeloAG.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split as split
+from keras.layers import LSTM, Input, Dense
+from keras.models import Model
+from deap import base, creator, tools, algorithms
+from scipy.stats import bernoulli
+from bitstring import BitArray
+
+np.random.seed(1120)
+data = pd.read_csv('train.csv')
+data = np.reshape(np.array(data['wp1']), (len(data['wp1']), 1))
+
+
+train_data = data[0:17257]
+test_data = data[17257:]
+
+def prepare_dataset(data, window_size):
+    X, Y = np.empty((0, window_size)), np.empty((0))
+    for i in range(len(data)-window_size-1):
+        X = np.vstack([X, data[i:(i + window_size), 0]])
+        Y = np.append(Y, data[i + window_size, 0])
+    
